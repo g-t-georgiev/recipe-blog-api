d4f1714a1a398adf2272e3dc5709ae96c13eef7b
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 18:28:03 +0200

    Implemented login action logic for data sanitization, validation, error handling, token creation and sending approriate cookie and json data.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,55 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
+const { AUTH_TOKEN_SECRET, AUTH_COOKIE } = require('../constants');
+
+let error;
+
+const login = async function (req, res, next) {
+    try {
+        let { 
+            user,
+            body: {
+                email,
+                password
+            }
+        } = req;
+
+        if (user) {
+            error = new Error('You are already logged in.');
+            error.statusCode = 403;
+            throw error;
+        }
+
+        email = email?.trim() ?? '';
+        password = password?.trim() ?? '';
+
+        const userData = await authService.login(email, password);
+        const accessToken = await authService.createToken(userData, AUTH_TOKEN_SECRET, { expiresIn: '1d' });
+        res.cookie(AUTH_COOKIE, accessToken);
+        res.status(200).json({ id: userData._id, username: userData.username, email: userData.email });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const register = async function (req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+const logout = async function (req, res, next) {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+router.post('/login', login);
+router.post('/register', register);
+router.delete('/logout', logout);
 
 module.exports = router;
