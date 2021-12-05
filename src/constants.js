1f8a0b241fb9fddb5d7f80285e8f2a5c61f3a114
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 16:29:53 +0200

    Added secret and auth header and cookie name.

--- src/constants.js
+++ src/constants.js
@@ -3,9 +3,16 @@ const PORT = process.env.PORT ?? 3030;
 const URL = process.env.URL ?? `http://localhost:${PORT}`;
 const DB = process.env.URL ?? 'mongodb://localhost:27017/recipe-blog';
 
+const AUTH_HEADER = 'authorization' || 'Authorization';
+const AUTH_COOKIE = 'auth';
+const AUTH_TOKEN_SECRET = '14187ec7791c6868d1c71b585b0296165ba6643175e6f73851933356ff77ba0c';
+
 module.exports = {
     NODE_ENV,
     PORT,
     URL,
-    DB
+    DB,
+    AUTH_HEADER,
+    AUTH_COOKIE,
+    AUTH_TOKEN_SECRET
 }
