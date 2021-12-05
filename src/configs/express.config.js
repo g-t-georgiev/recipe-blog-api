ab5f5b8b6155e111abda50f2d765cd4127337d0d
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 14:50:54 +0200

    Imported routes config in express config.

--- src/configs/express.config.js
+++ src/configs/express.config.js
@@ -2,9 +2,11 @@ module.exports = function (app) {
     const express = require('express');
     const cors = require('cors');
     const cookieParser = require('cookie-parser');
+    const routes = require('../routes');
 
     app.use(cors());
     app.use(cookieParser());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
+    app.use('/api', routes);
 }
