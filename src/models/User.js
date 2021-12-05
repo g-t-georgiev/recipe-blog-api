f79c1753f9244330b1fabfd5809bf42bb8dfde56
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 17:26:48 +0200

    Created user schema, model and setup hashing before save on register and verification of credentials on login.

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = require('../constants');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required.'],
+        minlength: [4, 'Username should be at least 4 characters long.'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required.'],
+        validate: [/.+@.+\..+/i, 'Invalid email format.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required.'],
+        min: [6, 'Password should be at least 6 characters long.']
+    },
+    _lastLoggedIn: {
+        type: Date,
+        default: 0
+    }
+}, 
+{ timestamps: {
+    createdAt: '_createdAt',
+    updatedAt: '_updatedAt'
+}});
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(SALT_ROUNDS);
+    const hash = await bcrypt.hash(this.password, salt);
+    this.password = hash;
+    return next();
+});
+
+userSchema.statics.verifyPassword = function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+const User = model('User', userSchema);
+
+module.exports = User;
