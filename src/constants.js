908332f8809e2b91437fe2ee836a79397c558924
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 15:19:44 +0200

    Declarared and exported constants separately.

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,11 @@
-exports.NODE_ENV = process.env.NODE_ENV ?? 'development';
-exports.PORT = process.env.PORT ?? 3030;
-exports.URL = process.env.URL ?? `http://localhost:${PORT}`;
-exports.DB = process.env.URL ?? 'mongodb://localhost:26017/recipe-blog';
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
+const PORT = process.env.PORT ?? 3030;
+const URL = process.env.URL ?? `http://localhost:${PORT}`;
+const DB = process.env.URL ?? 'mongodb://localhost:26017/recipe-blog';
+
+module.exports = {
+    NODE_ENV,
+    PORT,
+    URL,
+    DB
+}
