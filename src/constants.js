501cf82293bcfb0cf9d7f6c09b5c6db4b7c8775a
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 17:20:25 +0200

    Added salt rounds for bcrypt hashing.

--- src/constants.js
+++ src/constants.js
@@ -6,6 +6,7 @@ const DB = process.env.URL ?? 'mongodb://localhost:27017/recipe-blog';
 const AUTH_HEADER = 'authorization' || 'Authorization';
 const AUTH_COOKIE = 'auth';
 const AUTH_TOKEN_SECRET = '14187ec7791c6868d1c71b585b0296165ba6643175e6f73851933356ff77ba0c';
+const SALT_ROUNDS = 10;
 
 module.exports = {
     NODE_ENV,
@@ -14,5 +15,6 @@ module.exports = {
     DB,
     AUTH_HEADER,
     AUTH_COOKIE,
-    AUTH_TOKEN_SECRET
+    AUTH_TOKEN_SECRET,
+    SALT_ROUNDS
 }
