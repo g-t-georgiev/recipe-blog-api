6f2815903e48282388aaf6da66aebb31a27ff325
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 15:34:29 +0200

    Import error catcher middleware.

--- src/configs/express.config.js
+++ src/configs/express.config.js
@@ -3,10 +3,12 @@ module.exports = function (app) {
     const cors = require('cors');
     const cookieParser = require('cookie-parser');
     const routes = require('../routes');
+    const errorCatcherMiddleware = require('../middlewares/errorCatcherMiddleware');
 
     app.use(cors());
     app.use(cookieParser());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
     app.use('/api', routes);
+    app.use(errorCatcherMiddleware);
 }
