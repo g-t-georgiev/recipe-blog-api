6c8fb018a9dc2e46fcdd09907cd23e121471fa0e
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 15:20:55 +0200

    Change local db server port to the default one 27017.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const NODE_ENV = process.env.NODE_ENV ?? 'development';
 const PORT = process.env.PORT ?? 3030;
 const URL = process.env.URL ?? `http://localhost:${PORT}`;
-const DB = process.env.URL ?? 'mongodb://localhost:26017/recipe-blog';
+const DB = process.env.URL ?? 'mongodb://localhost:27017/recipe-blog';
 
 module.exports = {
     NODE_ENV,
