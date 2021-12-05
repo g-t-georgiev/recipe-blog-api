7176a9078510fefff564acc6448d5517595e3b98
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-05 16:31:15 +0200

    Added authentication middleware checking for access token in headers and cookies, also handling invalid and expired token errors.

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+module.exports = async function (req, res, next) {
+    try {
+        const { AUTH_HEADER, AUTH_COOKIE, AUTH_TOKEN_SECRET } = require('../constants');
+        const authService = require('../services/authService');
+        const accessToken = req.headers[AUTH_HEADER] || req.cookies[AUTH_COOKIE];
+
+        if (!accessToken) {
+            next();
+            return;
+        }
+
+        const payload = authService.verifyToken(accessToken, AUTH_TOKEN_SECRET);
+        payload.iat *= 1e3;
+        payload.exp *= 1e3;
+        req.user = payload;
+        next();
+    } catch (error) {
+        if (['TokenExpiredError', 'JsonWbTokenError'].includes(error.constructor.name)) {
+            error.statusCode = 401;
+        }
+
+        next(error);
+    }
+}
