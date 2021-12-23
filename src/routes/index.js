c0a380aeaa1c5a5357492e2e6b00985800d5efb8
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-23 02:52:55 +0200

    Added new route "users".

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const dataController = require('../controllers/dataController');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 router.use('/data', dataController);
 router.use('/auth', authController);
+router.use('/users', userController);
 
 module.exports = router;
