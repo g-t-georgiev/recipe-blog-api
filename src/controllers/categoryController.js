e85518f08a4b21c37e6ee4fd5ba1681988930905
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-23 16:43:30 +0200

    Added routes for fetching all or one category.

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
 
+const categoryService = require('../services/categoryService');
+
+const getAll = async function (req, res, next) {
+    try {
+        const categories = await categoryService.getAll(null, { title: 1 }, { lean: true });
+        res.status(200).json(categories);
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getOne = async function (req, res, next) {
+    try {
+        const { params: { categoryId }} = req;
+
+        const category = await categoryService.getOne(categoryId, { title: 1 }, { lean: true });
+        res.status(200).json(category);
+    } catch (error) {
+        next(error);
+    }
+}
+
+router.get('/', getAll);
+router.get('/:categoryId', getOne);
+
 module.exports = router;
