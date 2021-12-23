e7835744ae391348066813472cf3cc6335b72d8d
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-23 02:56:52 +0200

    Added recipe, category and review controllers.

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,3 +1,11 @@
 const router = require('express').Router();
 
+const recipeController = require('./recipeController');
+const categoryController = require('./categoryController');
+const reviewController = require('./reviewController');
+
+router.use('/recipes', recipeController);
+router.use('/categories', categoryController);
+router.use('/reviews', reviewController);
+
 module.exports = router;
