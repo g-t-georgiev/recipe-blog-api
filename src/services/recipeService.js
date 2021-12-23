1f3ff2214cb70a81faa07f6b90bbc56710ed719c
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-23 02:52:33 +0200

    Added recipe service for fetching from database.

---
+++ src/services/recipeService.js
@@ -0,0 +1,19 @@
+const Recipe = require('../models/Recipe');
+
+module.exports = {
+    get(filter = {}, projections = {}, options = {}) {
+        return Recipe.findOne(filter, { ...projections }, { ...options });
+    },
+    getById(id, projections = {}, options = {}) {
+        return Recipe.findById(id, { ...projections }, { ...options });
+    },
+    create(title, description, category, imageUrl, author) {
+        return Recipe.create({ title, description, category, imageUrl, author });
+    },
+    update(id, data, options = {}) {
+        return Recipe.findByIdAndUpdate(id, data, { ...options });
+    },
+    delete(id, options = {}) {
+        return Recipe.findByIdAndDelete(id, { ...options });
+    }
+};
