42269231c440d843a0e216dd3ea29d64924e2813
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-23 16:43:08 +0200

    Added get all and get one category service methods.

---
+++ src/services/categoryService.js
@@ -0,0 +1,10 @@
+const Category = require('../models/Category');
+
+module.exports = {
+    getAll(filter = {}, projections = {}, options = {}) {
+        return Category.find(filter, { ...projections }, { ...options });
+    },
+    getOne(id, projections = {}, options = {}) {
+        return Category.findById(id, { ...projections }, { ...options });
+    }
+};
