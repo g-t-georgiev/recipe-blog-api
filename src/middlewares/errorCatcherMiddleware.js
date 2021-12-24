a6b9ff70ef5694da43c85516112574775ba60122
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-25 00:47:47 +0200

    Refactored error response to support both multiple and single error messages.

--- src/middlewares/errorCatcherMiddleware.js
+++ src/middlewares/errorCatcherMiddleware.js
@@ -2,18 +2,22 @@ module.exports = function (error, req, res, next) {
     if (error) {
         const errorData = { message: '', multiple: false };
 
-        if (['ValidationError', 'CastError'].includes(error.constructor.name)) {
+        if (['ValidationError'].includes(error.constructor.name)) {
+            errorData.multiple = Object.keys(error).length > 1;
             const { errors } = error;
-            const messages = Object.keys(errors)
-                .map(path => errors[path].properties.message);
-            errorData.message = messages;
-            errorData.multiple = true;
+
+            if (['ValidatorError', 'CastError'].includes(errors[path].constructor.name)) {
+                let messages = [];
+
+                for (const path in errors) {
+                    messages.push(errors[path].properties.message);
+                }
+
+                errorData.message = messages.join(' ');
+            }
         } else {
             errorData.message = error.message;
-            errorData.multiple = false;
         }
-
-        // console.log(error.stack);
 
         res.status(error.statusCode ?? 500).json(errorData);
     }
