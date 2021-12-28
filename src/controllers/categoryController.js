9e36058d46a64e274a3f310230d5e3a4db317322
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-28 02:21:17 +0200

    Added default value of empty array for the category database query in get all categories action.

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -4,7 +4,8 @@ const categoryService = require('../services/categoryService');
 
 const getAll = async function (req, res, next) {
     try {
-        const categories = await categoryService.getAll(null, { title: 1 });
+        const categories = await categoryService.getAll(null, { title: 1 }) ?? [];
+        // console.log(categories);
         res.status(200).json(categories);
     } catch (error) {
         next(error);
