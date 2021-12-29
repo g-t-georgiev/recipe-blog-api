e101e092dc9c81923ed395c65a0661772f113ea4
g-t-georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2021-12-29 13:24:14 +0200

    Todo: fix bug on fetching private resources.

--- src/services/userService.js
+++ src/services/userService.js
@@ -29,16 +29,16 @@ module.exports = {
         return User.findOneAndUpdate({ _id: userId, favorites: recipeId }, { $pull: { favorites: recipeId } }, { ...options });
     },
     getRecipes(userId, projections = {}, options = {}) {
-        const recipes = Recipe.find({ author: userId }, { ...projections }, { ...options })
+        return Recipe.find({ author: userId }, { ...projections }, { ...options })
         .populate('favorites')?.favorites ?? [];
 
-        return Promise
-        .all(recipes
-        .map(async (recipe) => await Recipe.findById(recipe.id)
-            .populate({ path: 'author', select: 'username' })
-            .populate({ path: 'rating', select: 'rating recipe' })
-            .populate({ path: 'reviewCount', select: 'rating recipe' })
-        ));
+        // return Promise
+        // .all(recipes
+        // .map(async (recipe) => await Recipe.findById(recipe.id)
+        //     .populate({ path: 'author', select: 'username' })
+        //     .populate({ path: 'rating', select: 'rating recipe' })
+        //     .populate({ path: 'reviewCount', select: 'rating recipe' })
+        // ));
     },
     async isAuthorized(userId, recipeId) {
         const recipe = await Recipe.findById(recipeId)
